a8e6b492c8f95912273bf4006706d7eae4c01ded
Olivier Fedrigo <olivier.fedrigo@gmail.com>
2021-10-12 01:33:06 -0400

    directories

--- _fetch_NCBI.sh
+++ _fetch_NCBI.sh
@@ -7,22 +7,26 @@ taxa=$1
 
 script_path=$(dirname "$0")
 
+mkdir -p xml
+mkdir -p tsv
+mkdir -p filtered
+
 for taxon in ${taxa//,/ }
 do
 echo ">>> "$taxon
-if [ ! -f "${taxon}.xml" ]
+if [ ! -f "xml/${taxon}.xml" ]
 then
 echo "\t\tRetrieving ${taxon} genomes information from NCBI"
 query=${taxon}'[Organism]'
-esearch -db assembly -query $query | esummary > ${taxon}.xml
+esearch -db assembly -query $query | esummary > xml/${taxon}.xml
 fi
 
-sh ${script_path}/filter.sh ${taxon}.xml > ${taxon}.tsv
-echo "\t\t# of chromosome-scaffold scale assemblies: "$(cat ${taxon}.tsv | wc -l)
+sh ${script_path}/filter.sh xml/${taxon}.xml > tsv/${taxon}.tsv
+echo "\t\t# of chromosome-scaffold scale assemblies: "$(cat tsv/${taxon}.tsv | wc -l)
 
-python ${script_path}/format.py ${taxon}.tsv > ${taxon}_filtered.tsv
-echo "\t\t# of assemblies with VGP contiguity standards: "$(cat ${taxon}_filtered.tsv | wc -l)
+python ${script_path}/format.py tsv/${taxon}.tsv > filtered/${taxon}_filtered.tsv
+echo "\t\t# of assemblies with VGP contiguity standards: "$(cat filtered/${taxon}_filtered.tsv | wc -l)
 
 done
 
-cat *_filtered.tsv > final.tsv
+cat filtered/*_filtered.tsv > final.tsv
