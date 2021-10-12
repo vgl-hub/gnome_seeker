a0fabe1423644a8764eb63809dbc6ac6abc5774c
Olivier Fedrigo <olivier.fedrigo@gmail.com>
2021-10-12 00:29:30 -0400

    path update

--- _fetch_NCBI.sh
+++ _fetch_NCBI.sh
@@ -4,6 +4,9 @@ export PATH=${PATH}:$HOME/edirect
 
 taxa=$1
 
+
+script_path=$(dirname "$0")
+
 for taxon in ${taxa//,/ }
 do
 echo ">>> "$taxon
@@ -14,10 +17,10 @@ query=${taxon}'[Organism]'
 esearch -db assembly -query $query | esummary > ${taxon}.xml
 fi
 
-sh parse_xml.sh ${taxon}.xml > ${taxon}.tsv
+sh ${script_path}/filter.sh ${taxon}.xml > ${taxon}.tsv
 echo "\t\t# of chromosome-scaffold scale assemblies: "$(cat ${taxon}.tsv | wc -l)
 
-python3 parse.py ${taxon}.tsv > ${taxon}_filtered.tsv
+python ${script_path}/format.py ${taxon}.tsv > ${taxon}_filtered.tsv
 echo "\t\t# of assemblies with VGP contiguity standards: "$(cat ${taxon}_filtered.tsv | wc -l)
 
 done
--- format.py
+++ format.py
@@ -5,14 +5,6 @@ import sys,glob
 contigN50_min=1000000
 scaffoldN50_min=10000000
 
-
-vgplist=[]
-f=open("VGP.list")
-data=f.read().splitlines()
-f.close()
-for line in data: vgplist.append(line.split(" ")[0])
-
-
 fileName=sys.argv[1]
 
 f=open(fileName)
@@ -24,16 +16,10 @@ table=[]
 for line in data:
 	items=line.split("\t")
 	res={}
-	#print(items)
 	for item in items:
-		#print(item)
 		res[item.split(":")[0]]=item.split(":")[1]
-		#res["species"]=items[2].split(" (")[0]
-		#res["common"]=items[2].split(" (")[1].rstrip(")")
 	if float(res["contig_n50"])>=contigN50_min and float(res["scaffold_n50"])>=scaffoldN50_min:
 		table.append(res)
-
-#print(table)
 
 # collapse
 filtered={}
@@ -50,8 +36,5 @@ for taxId in filtered.keys():
 	filtered2.append(j)
 
 cols=["AssemblyAccession","AssemblyName","Organism","Taxid","assembly-status","SubmitterOrganization","representative-status","BioprojectAccn","contig_n50","scaffold_n50"]
-#print("\t".join(cols))
 for i in filtered2:
-	flag="non-VGP"
-	if i["AssemblyAccession"] in vgplist: flag="VGP"
-	print("\t".join([i[x] for x in cols]+[flag]))
+	print("\t".join([i[x] for x in cols]))
